804bae426d8cfd0bd20617789a37775708cacbd9
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-07 16:01:23 +0530

    login/RegisterPage Implemented

--- adminlogin-app/routes/web.php
+++ adminlogin-app/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminUserController;
 
 Route::get('/', function () {
-    return view('layout');
+    return view('loginRegister');
     
 });
 
